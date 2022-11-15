dc48b0d119f67402d4d03d9118bdd585d5cede8f
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-15 16:01:22 -0500

    footer & nav bar css

--- sanity-vdohkz-timon/components/Navbar.jsx
+++ sanity-vdohkz-timon/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Link from "next/link";
+import { AiOutlineShopping } from "react-icons/ai";
+
+// import { Cart } from './';
+// import { useStateContext} from '../context/StateContext';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">JSM Headphones</Link>
+      </p>
+
+      <button type="button" className="cart-icon" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+
+      {/* {showCart && <Cart />} */}
+    </div>
+  );
 };
 
 export default Navbar;
