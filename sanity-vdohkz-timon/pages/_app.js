4953dfae85f16116f87766b872564ba0f359b7de
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-16 16:29:58 -0500

    cart slide winow, incr decr func

--- sanity-vdohkz-timon/pages/_app.js
+++ sanity-vdohkz-timon/pages/_app.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Toaster } from "react-hot-toast";
 
 import { Layout } from "../components";
 import "../styles/globals.css";
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
