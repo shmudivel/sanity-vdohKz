50e263cba04345b7cebdb5ec16ffa253c32c17bf
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-17 15:52:17 -0500

    sripe done

--- sanity-vdohkz-timon/pages/success.js
+++ sanity-vdohkz-timon/pages/success.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
 
 import { useStateContext } from "../context/StateContext";
-// import { runFireworks } from "../lib/utils";
+import { runFireworks } from "../lib/utils";
 
 const Success = () => {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
@@ -13,7 +13,7 @@ const Success = () => {
     setCartItems([]);
     setTotalPrice(0);
     setTotalQuantities(0);
-    // runFireworks();
+    runFireworks();
   }, []);
 
   return (
