405e411b22b1254a27c9d9bdd2126cf6aa39c55d
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-24 22:48:01 -0500

    USD to KZT

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -124,7 +124,7 @@ const Cart = () => {
           <div className="cart-bottom">
             <div className="total">
               <h3>Subtotal:</h3>
-              <h3>${totalPrice}</h3>
+              <h3>₸{totalPrice}</h3>
             </div>
             <div className="btn-container">
               <button type="button" className="btn" onClick={handleCheckout}>
