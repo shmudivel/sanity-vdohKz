d7a353c0c394606ba85c0f5acea12d985b46a4bc
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-11-26 17:39:43 -0500

    social media links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import { BsInstagram, BsTelegram, BsWhatsapp } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 VdohKz All rights reserverd</p>
+      <p>2022 Vdoh All rights reserverd</p>
       <p className="icons">
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href="https://www.instagram.com/vdoh_kz/">
+          <BsInstagram />
+        </a>
+        <a href="https://t.me/vdohkz">
+          <BsTelegram />
+        </a>
+        {/* <a href="/">
+          <BsWhatsapp />
+        </a> */}
       </p>
     </div>
   );
